f3e784ffa0d4cd556844ffbf7c8f8ea4d4d44f2b
Devi-saranya5548 <155404986+Devi-saranya5548@users.noreply.github.com>
2024-07-23 15:52:51 +0530

    TestCommit

--- assignment1.js
+++ assignment1.js
@@ -4,7 +4,7 @@ function Ass1() {
     var names = ["Scott Ayres","Richard Beson" , "Jacob Hilpertshauser","Stephanie Sweet" , "Sarah Hecker","Curt Ziegler","Hannah Recker", "Michael Angiletta"];
     var pos = ["Busting social media myths at the Social Media Lab","SaaS Customer Onboarding Manager / Producer and ...","Harness your social media content and engageme...","Customer Support / Software QA Tester","US Support & Affiliate Manager at Agorapulse","Customer Support Hero at Agorapulse","Inbound & Marketing Operations Manager at ...","Growth Marketing | CMO | Agorapulse"];
     var no = [23,7,5,4,3,3,4,3];
-
+ 
     return (
         <div id="container">
             {names.map((name, index) => 
