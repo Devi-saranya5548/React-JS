b8f05fd7cf6cad4edc6e6a8af4dc60a960adc5ea
Devi-saranya5548 <155404986+Devi-saranya5548@users.noreply.github.com>
2024-07-23 08:42:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from 'react';
+import Ass1 from "./assignment1";
+import "./style.css";
+
+const rootElement = document.getElementById('root');
+const rootRef = ReactDOM.createRoot(rootElement);
+rootRef.render(<Ass1 />);
