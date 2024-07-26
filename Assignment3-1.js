b0f6ed2079aa982f3c5015e1520a59d1f21e83c7
Devi-saranya5548 <155404986+Devi-saranya5548@users.noreply.github.com>
2024-07-26 08:42:09 +0530

    COMMITzo0us77ku

--- Assignment3-1.js
+++ Assignment3-1.js
@@ -5,7 +5,7 @@ const Product =({product}) => {
             <img src={product.image} alt={product.title}/>
             <h3>{product.title}</h3>
             <p>{product.price}</p>
-            <button>Add to Cart</button>
+            <button>Add to Cart</button> 
         </div>
     )
 }
